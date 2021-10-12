f7caeeed62d537708c01d5b2206b7d41e62c03e1
Darkphilip <yoonlucky0@gmail.com>
2021-10-12 14:13:56 +0900

    study struct

---
+++ src/section8/struct4.go
@@ -0,0 +1,19 @@
+// 구조체 기본(4)
+package main
+
+import "fmt"
+
+func main() {
+	// 구조체 익명 선언 및 활용
+	// 예제1 type 구조체명 타입
+	car1 := struct{ name, color string }{"520d", "red"}
+	fmt.Println("ex1:", car1)
+	fmt.Printf("ex1: %#v\n", car1)
+
+	// 예제2
+	cars := []struct{ name, color string }{{"520d", "red"}, {"528i", "white"}, {"320d", "blue"}}
+
+	for _, c := range cars {
+		fmt.Printf("(%s, %s) ----- (%#v)\n", c.name, c.color, c)
+	}
+}
