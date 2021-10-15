53dacd33a450a44444ca8f1a5153c81ab5d63c08
Darkphilip <yoonlucky0@gmail.com>
2021-10-15 14:18:17 +0900

    study 구조체 심화

---
+++ src/section8/struct_ex2.go
@@ -0,0 +1,35 @@
+// 구조체 심화(2)
+package main
+
+import "fmt"
+
+type Account struct {
+	number   string
+	balance  float64
+	interest float64
+}
+
+func CalcalateD(a Account) {
+	a.balance = a.balance + (a.balance * a.interest)
+}
+
+func CalcalateP(a *Account) {
+	a.balance = a.balance + (a.balance * a.interest)
+}
+
+func main() {
+	// 예제1
+	kim := Account{"245-901", 10000000, 0.015}
+	lee := Account{"245-901", 20000000, 0.025}
+
+	fmt.Println("ex1:", kim)
+	fmt.Println("ex1:", lee)
+	fmt.Println()
+
+	CalcalateD(kim)
+	CalcalateP(&lee)
+
+	fmt.Println("ex2:", int(kim.balance))
+	fmt.Println("ex2:", int(lee.balance))
+
+}
